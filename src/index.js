88e18064180e577653f56b4358845029c5f17bca
Sarbojit <sarbojitrana47c@gmail.com>
2025-09-26 20:08:04 +0530

    connect mongoDB and setup done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 
+import express from  "express";
+
+const app = express() ;
+
 
 ;( async ()=>{
     try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        app.on("error", (error) =>{
+            console.log("ERROR : ", error);
+            throw error;
+        });
+        
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is running on port ${process.env.PORT}`);
+        });
+
     } catch(error){
         console.error("ERROR : ", error);
-        throw error
+        throw error;
     }
 })()
