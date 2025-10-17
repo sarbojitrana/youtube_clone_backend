6f38e4072c7f811233db79f9d80ec0b7625882c9
sarbojitrana <sarbojitrana47c@gmail.com>
2025-10-17 12:46:38 +0530

    fix multer path bug

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,5 @@
 import multer from 'multer';
-
+import path from 'path'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     const originalNameWithoutExt = path.parse(file.originalname).name
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() *159);
     const extension = path.extname(file.originalname)
-    cb(null, file.originalname + '-' + uniqueSuffix + extension);
+    cb(null, originalNameWithoutExt + '-' + uniqueSuffix + extension);
   }
 })
 
